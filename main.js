a02917345b0c299f5a20b7eb258f04f3d850a65d
Sinan Pasic <sinan.p.pasic@gmail.com>
2023-03-26 20:09:19 +0200

    refactor

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
 const SIZE = 1000;
+const TAU = 2 * Math.PI;
+
+let points;
 
 const circle_point = (angle_rad, radius) => ({
-    x: (canvas.width / 2) + radius + Math.cos(angle_rad),
-    y: (canvas.height / 2) + radius + Math.sin(angle_rad),
+    x: (canvas.width / 2) + radius * Math.cos(angle_rad),
+    y: (canvas.height / 2) + radius * Math.sin(angle_rad),
 });
 
 function mk_circle_points(radius, point_count) {
     let points = [];
 
-    for (let angle_rad = 0; angle_rad < 2 * Math.PI; angle_rad += 2 * Math.PI / point_count)
+    for (let angle_rad = 0; angle_rad < TAU; angle_rad += TAU / point_count)
         points.push(circle_point(angle_rad, radius));
 
     return points;
@@ -27,7 +30,7 @@ function draw_circle_outline(radius) {
     ctx.strokeStyle = "Gray";
 
     ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
+    ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, TAU);
     ctx.stroke();
 
     ctx.closePath();
@@ -38,13 +41,13 @@ function draw_circle_points(points) {
 
     points.forEach(function(point) {
         ctx.beginPath();
-        ctx.arc(point.x, point.y, 5, 0, 2 * Math.PI);
+        ctx.arc(point.x, point.y, 5, 0, TAU);
         ctx.fill();
         ctx.closePath();
     });
 }
 
-let points = mk_circle_points(450, 100);
 init_canvas();
+points = mk_circle_points(450, 100);
 draw_circle_outline(450);
 draw_circle_points(points);
